7b7bfffbee513dde8edcdacb5cb041fcddd7120b
yishakGursha <yisehakgursha.dventus@gmail.com>
2024-05-20 11:50:56 +0300

    Initial commit

---
+++ my-app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Heading from './Heading';
+import './index.css';
+
+ReactDOM.render(
+  <div>
+    <Heading />
+    <ul>
+      <li>shiro</li>
+      <li>tegabino</li>
+      <li>kitfo</li>
+    </ul>
+  </div>,
+  document.getElementById('root')
+);
+
